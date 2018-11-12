d9735a34daa99f6af2fe7313e4473a87aa882ee6
yanghao <389812249@qq.com>
2018-11-12 10:26:09 +0800

    @

--- index.php
+++ index.php
@@ -4,11 +4,12 @@
  * @author yanghao
  * @date 2018-10-18
  */
+error_reporting(E_ALL);
 define('ENV', 'online');
 if(ENV == 'local'){
     require __DIR__.'/../hlw_php/ApiCore.php';
 } else {
-    require realpath( __DIR__).'/ApiCore.php';
+    require realpath('/home/wwwroot/hlw_php').'/ApiCore.php';
 }
 
 ApiCore::init('app', 'hlwApiSdk');
