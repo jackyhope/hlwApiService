11d771762f476d0324d4ff9ade8b4c659674077a
guoqingsong <1020074734@qq.com>
2019-06-25 11:08:40 +0800

    ###ip更改

--- app/conf/db.local.php
+++ app/conf/db.local.php
@@ -3,8 +3,8 @@
 main   =   host:192.168.0.250,port:3306,user:root,database:huiliewang,password:123456,charset:utf8
 query  =   host:192.168.0.250,port:3306,user:root,database:huiliewang,password:123456,charset:utf8
 [mx]
-main   =   host:192.168.0.250,port:3306,user:root,database:pinping,password:123456,charset:utf8
-query  =   host:192.168.0.250,port:3306,user:root,database:pinping,password:123456,charset:utf8
+main   =   host:192.168.0.248,port:3306,user:root,database:pinping,password:123456,charset:utf8
+query  =   host:192.168.0.248,port:3306,user:root,database:pinping,password:123456,charset:utf8
 [resume]
 main   =   host:192.168.0.250,port:3306,user:root,database:resume,password:123456,charset:utf8
 query  =   host:192.168.0.250,port:3306,user:root,database:resume,password:123456,charset:utf8
