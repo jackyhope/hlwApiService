7fc2e37d1c076039e9e08d704cab722288964812
guoqingsong <1020074734@qq.com>
2019-06-25 15:58:07 +0800

    ###添加 DB配置

--- app/conf/db.local.php
+++ app/conf/db.local.php
@@ -8,3 +8,6 @@ query  =   host:192.168.0.248,port:3306,user:root,database:pinping,password:1234
 [resume]
 main   =   host:192.168.0.250,port:3306,user:root,database:resume,password:123456,charset:utf8
 query  =   host:192.168.0.250,port:3306,user:root,database:resume,password:123456,charset:utf8
+[pinping]
+main   =   host:192.168.0.248,port:3306,user:root,database:pinping,password:123456,charset:utf8
+query  =   host:192.168.0.248,port:3306,user:root,database:pinping,password:123456,charset:utf8
