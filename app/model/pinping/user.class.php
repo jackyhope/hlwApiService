0d6d4f73693d413d03a0a999648689802c804c2e
langhonglian <626724464@qq.com>
2019-06-26 09:54:32 +0800

    profession_type

--- app/model/pinping/user.class.php
+++ app/model/pinping/user.class.php
@@ -56,10 +56,11 @@ class model_pinping_user extends hlw_components_basemodel
         $department > 0 && $roles = $this->departmentRoles($department);
         $roles && $where .= " and role_id in ({$roles})";
         $name && $where .= " and full_name like '%{$name}%' ";
-        $type && $where .= " and type = {$type} ";
+        $type && $where .= " and profession_type = {$type} ";
 
         $item = "user_id,full_name,role_id,status,type,job_rank,profession_type,name,full_name,entry,graduation_time";
         $list = $this->select($where, $item, '', "order by user_id desc");
+        return $where;
         $counts = $list->totalSize;
         $currentPage = $list->page;
         $pageSize = $list->pageSize;
