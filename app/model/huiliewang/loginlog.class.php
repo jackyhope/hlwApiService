ae5e306dc28f2a67706a9af4643cf1e5b11c28d1
hellocrab <15215087350@163.com>
2019-07-13 17:26:44 +0800

    model_huiliewang_loginlog

---
+++ app/model/huiliewang/loginlog.class.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class model_huiliewang_loginlog extends hlw_components_basemodel
+{
+
+    public function primarykey() {
+        return 'id';
+    }
+
+    public function tableName() {
+        return 'phpyun_login_log';
+    }
+
+}
