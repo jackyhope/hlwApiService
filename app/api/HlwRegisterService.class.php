649db67772b91a1d8bdd53a41391231b46290b27
guoqingsong <1020074734@qq.com>
2019-07-16 09:54:52 +0800

    ###注册入OA，慧猎网company表member表一致

--- app/api/HlwRegisterService.class.php
+++ app/api/HlwRegisterService.class.php
@@ -15,8 +15,8 @@ class api_HlwRegisterService extends api_Abstract implements \com\hlw\huiliewang
     {
         // TODO: Implement checkTel() method.
         $resultDo = new ResultDO();
-        $company = new model_huiliewang_company();
-        $data = $company->selectOne('linktel = '.$tel);
+        $company = new model_huiliewang_member();
+        $data = $company->selectOne(['moblie'=>$tel]);
         if(empty($data)){
             $resultDo->success = true;
             $resultDo->code = 200;
@@ -70,11 +70,7 @@ class api_HlwRegisterService extends api_Abstract implements \com\hlw\huiliewang
             $arr['passtext'] = $invite;
             $status = $member->insert($arr);
             $member_id = $member->lastInsertId();
-            //接有邀请码的 有company保存BD信息
-            if(!empty($invite)){
-                $data = $user->selectOne(['identity'=>1,'invitecode'=>intval($invite)],'role_id,full_name');
-                $company->update(['uid'=>$member_id],['con_oa_userroleid'=>intval($data['role_id']),'con_oa_username'=>$data['full_name']]);
-            }
+
             if($status){
                 $customer = new model_pinping_customer();
                 $customer_data = new model_pinping_customerdata();
@@ -98,6 +94,24 @@ class api_HlwRegisterService extends api_Abstract implements \com\hlw\huiliewang
                 ];
                 $customer->insert($customer_ins);
                 $customer_id = $customer->lastInsertId();
+                if(!empty($invite)){ //接有邀请码的 company保存BD信息
+                    $data = $user->selectOne(['identity'=>1,'invitecode'=>intval($invite)],'role_id,full_name');
+                    $arr_com = [
+                        'con_oa_userroleid'=>intval($data['role_id']),
+                        'con_oa_username'=>$data['full_name'],
+                        'tb_customer_id'=>$customer_id,
+                        'uid'=>$member_id,
+                        'linktel'=>$tel
+                    ];
+                    $company->insert($arr_com);
+                }else{
+                    $arr_com = [
+                        'tb_customer_id'=>$customer_id,
+                        'uid'=>$member_id,
+                        'linktel'=>$tel
+                    ];
+                    $company->insert($arr_com);
+                }
                 $customer_data_ins = [
                     'customer_id' => $customer_id,
                     'money' => '',
