ee3119844b68c2429c8c16a0b310bf6715f181e2
yanghao <389812249@qq.com>
2019-07-31 19:05:13 +0800

    @

--- app/conf/db.product.php
+++ app/conf/db.product.php
@@ -1,10 +1,10 @@
 ;<?php exit(); ?>
 [huiliewang]
-main   =   host:10.30.88.15,port:3306,user:SAP,database:huiliewang,password:bffebfb01900fe3af8a8633d3b0b7be2,charset:utf8
-query  =   host:10.30.88.15,port:3306,user:SAP,database:huiliewang,password:bffebfb01900fe3af8a8633d3b0b7be2,charset:utf8
+main   =   host:47.106.49.109,port:33062,user:SAP,database:huiliewang,password:bffebfb01900fe3af8a8633d3b0b7be2,charset:utf8
+query  =   host:47.106.49.109,port:33063,user:SAP,database:huiliewang,password:bffebfb01900fe3af8a8633d3b0b7be2,charset:utf8
 [pinping]
 main   =   host:47.106.49.109,port:33062,user:root,database:pinping,password:bffebfb01900fe3af8a8633d3b0b7be2,charset:utf8
 query  =   host:47.106.49.109,port:33062,user:root,database:pinping,password:bffebfb01900fe3af8a8633d3b0b7be2,charset:utf8
 [resume]
-main   =   host:10.30.88.15,port:3306,user:SAP,database:resume,password:bffebfb01900fe3af8a8633d3b0b7be2,charset:utf8
-query  =   host:10.30.88.15,port:3306,user:SAP,database:resume,password:bffebfb01900fe3af8a8633d3b0b7be2,charset:utf8
+main   =   host:47.106.49.109,port:33062,user:SAP,database:resume,password:bffebfb01900fe3af8a8633d3b0b7be2,charset:utf8
+query  =   host:47.106.49.109,port:33063,user:SAP,database:resume,password:bffebfb01900fe3af8a8633d3b0b7be2,charset:utf8
