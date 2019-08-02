8dbd1aa234b73ec74054781b713abb06c99852d1
guoqingsong <1020074734@qq.com>
2019-08-02 14:31:43 +0800

    ####

--- app/api/AdminManagerService.class.php
+++ app/api/AdminManagerService.class.php
@@ -15,20 +15,21 @@ class api_AdminManagerService extends api_Abstract implements com\hlw\huiliewang
         // TODO: Implement bdList() method.
         $user = new model_pinping_user();
         $resultDo = new ResultDO();
-        $userList = $user -> select(['identity'=>1],'role_id,full_name,invitecode')->items;
+        $userList = $user -> select(['identity' =>1],'role_id,full_name,invitecode')->items;
         $resultDo->code = 200;
         $resultDo->success = true;
         $resultDo->data = $userList;
         $resultDo->message = '查询成功';
         return $resultDo;
     }
+
     //查询BD
     public function searBD($name)
     {
         // TODO: Implement searBD() method.
         $user = new model_pinping_user();
         $resultDo = new ResultDO();
-        $userList = $user -> select('identity = 1 and full_name like "%'.$name.'%"','role_id,full_name,invitecode')->items;
+        $userList = $user->select('identity = 1 and full_name like "%' . $name . '%"', 'role_id,full_name,invitecode')->items;
         $resultDo->code = 200;
         $resultDo->success = true;
         $resultDo->data = $userList;
@@ -45,34 +46,36 @@ class api_AdminManagerService extends api_Abstract implements com\hlw\huiliewang
         $company = new model_huiliewang_company();
         $customer = new model_pinping_customer();
         $customerdata = new model_pinping_customerdata();
-        $res = $company->update(['uid'=>$uid],['addtime'=>$time,'conid'=>$rid,'con_oa_userroleid'=>$rid,'con_oa_username'=>$rname]);
-        if(empty($res)){
+        $res = $company->update(['uid' => $uid], ['addtime' => $time, 'conid' => $rid, 'con_oa_userroleid' => $rid, 'con_oa_username' => $rname]);
+        if (empty($res)) {
             $resultDo->code = 200;
             $resultDo->message = '分配成功';
             $resultDo->success = true;
             return $resultDo;
+
         }
-        if($res){
-            $data = $company->selectOne(['uid'=>$uid],'tb_customer_id,name');
+
+        if ($res) {
+            $data = $company->selectOne(['uid' => $uid], 'tb_customer_id,name');
 //            $da = $customer->insert(['customer_owner_id'=>$rid,'owner_role_id'=>$rid,'name'=>$data['username'],'creator_role_id'=>$rid,'origin'=>'线下慧简历','is_locked'=>1,'introduce'=>'adsfa','location'=>'dafsa','crm_vfagxj'=>'dsaf']);
 //            $dataid = $customerdata->insert(['customer_id'=>intval($da)]);
-            if(!empty($data['tb_customer_id'])){
-                $re = $customer->update(['customer_id'=>intval($data['tb_customer_id'])],['customer_owner_id'=>$rid,'owner_role_id'=>$rid,'creator_role_id'=>$rid,'update_time'=>time(),'is_locked'=>1]);
-                if($re){
+            if (!empty($data['tb_customer_id'])) {
+                $re = $customer->update(['customer_id' => intval($data['tb_customer_id'])], ['customer_owner_id' => $rid, 'owner_role_id' => $rid, 'creator_role_id' => $rid, 'update_time' => time(), 'is_locked' => 1]);
+                if ($re) {
                     $resultDo->code = 200;
                     $resultDo->message = '分配成功';
                     $resultDo->success = true;
-                }else{
+                } else {
                     $resultDo->code = 500;
                     $resultDo->message = '分配失败,该BD已被分配';
                     $resultDo->success = false;
                 }
-            }else{
+            } else {
                 $resultDo->code = 500;
                 $resultDo->message = '分配失败,需完整客户名称等资料';
                 $resultDo->success = false;
             }
-        }else{
+        } else {
             $resultDo->code = 500;
             $resultDo->message = '分配失败';
             $resultDo->success = false;
@@ -89,16 +92,16 @@ class api_AdminManagerService extends api_Abstract implements com\hlw\huiliewang
         $resultDO = new ResultDO();
         $member = new model_huiliewang_member();
         $customer = new model_pinping_customer();
-        $customerr = $member->selectOne(['uid'=>$uid],'tb_customer_id');
-        $res = $member->update(['uid'=>$uid],['moblie'=>$phone]);
-        if($res){
-            $flag = $customer->update(['customer_id'=>intval($customerr['tb_customer_id'])],['telephone'=>$phone]);
+        $customerr = $member->selectOne(['uid' => $uid], 'tb_customer_id');
+        $res = $member->update(['uid' => $uid], ['moblie' => $phone]);
+        if ($res) {
+            $flag = $customer->update(['customer_id' => intval($customerr['tb_customer_id'])], ['telephone' => $phone]);
         }
-        if($flag){
+        if ($flag) {
             $resultDO->code = 200;
             $resultDO->success = true;
             $resultDO->message = '修改成功！';
-        }else{
+        } else {
             $resultDO->code = 500;
             $resultDO->success = false;
             $resultDO->message = '修改失败！';
@@ -117,12 +120,12 @@ class api_AdminManagerService extends api_Abstract implements com\hlw\huiliewang
         // TODO: Implement checkphone() method.
         $resultDO = new ResultDO();
         $member = new model_huiliewang_member();
-        $bphone = $member->selectOne(['uid'=>$uid],'moblie');
-        if($phone == $bphone['moblie']){
+        $bphone = $member->selectOne(['uid' => $uid], 'moblie');
+        if ($phone == $bphone['moblie']) {
             $resultDO->code = 200;
             $resultDO->success = true;
             $resultDO->message = '手机号正确！';
-        }else{
+        } else {
             $resultDO->code = 500;
             $resultDO->success = false;
             $resultDO->message = '你的原手机号不正确！';
