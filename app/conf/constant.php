02b4ec05fe2ebb13fb471285822a10c076ad1cde
hellocrab <15215087350@163.com>
2019-08-08 14:50:52 +0800

    新增配置常量

--- app/conf/constant.php
+++ app/conf/constant.php
@@ -11,3 +11,30 @@ if (strpos($_SERVER['SERVER_ADDR'], '192.168') !== FALSE || strpos($_SERVER['SER
 } else {
     define('OA_ROLE', 1271);
 }
+/**2019-08-08  add **/
+define('new_price',array(
+        'communicate_base_position'=>300,
+        'communicate_expert_position'=>500,
+        'interview'=>array(
+            'base'=>array(
+                'price'=>1000,
+                'interval'=>'0-20',
+                'giving'=>0.5,
+                'start_buy'=>10000
+            ),
+            'Intermediate'=>array(
+                'price'=>1000,
+                'interval'=>'20-50',
+                'giving'=>0.5,
+                'start_buy'=>10000
+            ),
+            'expert'=>array(
+                'price'=>1000,
+                'interval'=>'50-9999999',
+                'giving'=>0.5,
+                'start_buy'=>10000
+            )
+        ),
+    )
+);
+/**2019-08-08  add **/
